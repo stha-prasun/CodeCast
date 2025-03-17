6213f4102834215a354a91d3f7f84c70261fa5e5
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-03-17 17:07:05 +0545

    Login And Signup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,95 @@
+import { User } from "../models/userSchema.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullname, email, password } = req.body;
+    if (!fullname || !email || !password) {
+      return res.status(400).json({
+        message: "Fields cannot be left empty!!",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        message: "Email Already In Use!!",
+        success: false,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      message: "User created successfully!!",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "Fields cannot be left empty!!",
+      success: false,
+    });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid User",
+      success: false,
+    });
+  }
+
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatched) {
+    return res.status(400).json({
+      message: "Incorrect Email or Password",
+      success: false,
+    });
+  }
+
+  const tokenData = {
+    userID: user._id,
+  };
+
+  const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+
+  const loggedInUseruser = {
+    _id: user._id,
+    fullname: user.fullname,
+    email: user.email,
+  };
+
+  return res
+    .status(201)
+    .cookie("token", token, {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+    })
+    .json({
+      message: `Welcome Back ${user.fullname}!!`,
+      success: true,
+      loggedInUseruser,
+    });
+};
---
+++ backend/models/codeSnippetSchema.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const codeSnippetSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const CodeSnippet = mongoose.model("CodeSnippet", codeSnippetSchema);
+export default CodeSnippet;
