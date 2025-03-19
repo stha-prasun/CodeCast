b8e1b9ef4e6fa41fc45f2d74ef76f1d052af105d
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-03-19 09:26:17 +0545

    Signup API Integration

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,66 +1,120 @@
 import React from "react";
-import { Link } from "react-router";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { USER_API_ENDPOINT } from "../../utils/constants";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Signup = () => {
-    return (
-      <div className="min-h-screen flex justify-center items-center bg-base-200">
-        <div className="card w-full max-w-md shadow-xl bg-base-100 p-8">
-          <h2 className="text-3xl font-semibold text-center text-primary">Sign Up</h2>
-  
-          <form className="space-y-4 mt-6">
-            {/* Full Name */}
-            <div>
-              <label className="label">
-                <span className="label-text">Full Name</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Enter your full name"
-                className="input input-bordered w-full"
-              />
-            </div>
-  
-            {/* Email */}
-            <div>
-              <label className="label">
-                <span className="label-text">Email Address</span>
-              </label>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="input input-bordered w-full"
-              />
-            </div>
-  
-            {/* Password */}
-            <div>
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                className="input input-bordered w-full"
-              />
-            </div>
-  
-            {/* Submit Button */}
-            <div className="mt-6">
-              <button className="btn btn-primary w-full">Sign Up</button>
-            </div>
-  
-            {/* Already have an account */}
-            <div className="text-center mt-4">
-              <p>
-                Already have an account?{" "}
-                <Link to="/login" className="text-primary">Login here</Link>
-              </p>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
+  const navigate = useNavigate();
+  const [input, setinput] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setinput({ ...input, [e.target.name]: e.target.value });
   };
-  
-  export default Signup;
-  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${USER_API_ENDPOINT}/signup`,
+        {
+          fullname: input.fullname,
+          email: input.email,
+          password: input.password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  };
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-base-200">
+      <div className="card w-full max-w-md shadow-xl bg-base-100 p-8">
+      <h2 className="text-2xl font-bold text-center mb-6">Sign Up</h2>
+
+        <form onSubmit={handleSubmit} className="space-y-4 mt-6">
+          {/* Full Name */}
+          <div>
+            <label className="label">
+              <span className="label-text">Full Name</span>
+            </label>
+            <input
+              value={input.fullname}
+              onChange={handleChange}
+              type="text"
+              name="fullname"
+              placeholder="Enter your full name"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          {/* Email */}
+          <div>
+            <label className="label">
+              <span className="label-text">Email Address</span>
+            </label>
+            <input
+              value={input.email}
+              onChange={handleChange}
+              type="text"
+              name="email"
+              placeholder="Enter your email"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          {/* Password */}
+          <div>
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              value={input.password}
+              onChange={handleChange}
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          {/* Submit Button */}
+          <div className="mt-6">
+            <button type="submit" className="btn btn-primary w-full">
+              Sign Up
+            </button>
+          </div>
+
+          {/* Already have an account */}
+          <div className="text-center mt-4">
+            <p>
+              Already have an account?{" "}
+              <Link to="/login" className="text-primary">
+                Login here
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
