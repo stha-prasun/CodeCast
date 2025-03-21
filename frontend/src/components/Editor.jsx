8655951fc9142aca7040f2490b7463e6d06762e4
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-03-21 09:28:57 +0545

    codeChange Event

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -1,24 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MonacoEditor from "@monaco-editor/react";
 import Navbar from "./shared/Navbar";
 import LeftSidebar from "./LeftSidebar";
 import io from "socket.io-client";
+
 const socket = io.connect("http://localhost:8080");
 
 const Editor = () => {
   const [code, setCode] = useState("");
 
+  useEffect(() => {
+    // Listen for code updates from the server
+    socket.on("codeUpdate", (newCode) => {
+      setCode(newCode);
+    });
+
+    // Cleanup on component unmount
+    return () => {
+      socket.off("codeUpdate");
+    };
+  }, []);
+
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
+    socket.emit("codeChange", newCode);
+  };
+
   return (
     <div className="box-border h-screen flex flex-col">
       <Navbar />
-      <div className="w-full flex-1 flex overflow-hidden"> {/* This will allow the editor to fit the space */}
+      <div className="w-full flex-1 flex overflow-hidden">
         {/* Sidebar */}
         <LeftSidebar />
 
         {/* Monaco Editor */}
         <div className="flex-1 p-4 overflow-hidden">
           <MonacoEditor
-            height="88vh"  // Set height to viewport height
+            height="88vh"
             language="javascript"
             theme="vs-dark"
             options={{
@@ -27,7 +45,7 @@ const Editor = () => {
               fontFamily: "'Courier New', monospace",
             }}
             value={code}
-            onChange={(newValue) => setCode(newValue)}
+            onChange={handleCodeChange} //
           />
         </div>
       </div>
