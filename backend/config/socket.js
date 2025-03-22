bc7eb439a2104f9370be9d417715303e227c6524
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-03-22 13:11:06 +0545

    Active Users List

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -1,4 +1,7 @@
 import { Server } from "socket.io";
+import { User } from "../models/userSchema.js";
+
+const activeUsers = new Map();
 
 const initializeSocket = (server) => {
   const io = new Server(server, {
@@ -12,8 +15,25 @@ const initializeSocket = (server) => {
   io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
+    socket.on("userJoined", async (userId) => {
+      try {
+        const user = await User.findById(userId).select("profilePic");
+        if (!user) return;
+
+        // Store user in activeUsers map
+        activeUsers.set(socket.id, { profilePic: user.profilePic });
+
+        // Send updated active users list to all clients
+        io.emit("activeUsers", Array.from(activeUsers.values()));
+
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    });
+
     socket.on('codeChange', (data) => {
       console.log(data);
+      socket.broadcast.emit("codeUpdate", data);
     });
 
     socket.on("disconnect", () => {
