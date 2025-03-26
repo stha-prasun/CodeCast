b948076f7aa5f6d28c9e14da930d9fbb97b335a8
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-03-26 09:23:20 +0545

    Snippets

--- frontend/src/components/SnippetCard.jsx
+++ frontend/src/components/SnippetCard.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import toast from "react-hot-toast";
 
 const SnippetCard = ({ snippet }) => {
+  const handleCopy = async () =>{
+    await navigator.clipboard.writeText(snippet?.code);
+    toast.success("Code Copied To Clipboard");
+  }
   return (
     <div className="bg-base-200 p-4 border border-gray-300 rounded-md flex flex-col gap-2">
-      <h2 className="font-medium text-lg">{snippet.title}</h2>
+      <h2 className="font-medium text-lg">{snippet?.title}</h2>
+      <h3 className="font-medium text-lg text-gray-600">{snippet?.description}</h3>
       <pre className="bg-gray-900 text-white p-3 rounded-md text-sm overflow-x-auto">
-        <code>{snippet.code}</code>
+        <code>{snippet?.code}</code>
       </pre>
-      <button className="btn btn-sm btn-primary self-start">
-        View In Editor
+      <button onClick={handleCopy} className="btn btn-sm btn-outline btn-success self-start">
+        Copy Code
       </button>
     </div>
   );
--- frontend/src/components/SnippetsList.jsx
+++ frontend/src/components/SnippetsList.jsx
@@ -1,37 +1,18 @@
 import React from "react";
 import Navbar from "./shared/Navbar";
 import SnippetCard from "./SnippetCard";
-
-const snippets = [
-  {
-    id: 1,
-    title: "React useState Example",
-    language: "JavaScript",
-    code: "const [count, setCount] = useState(0);",
-  },
-  {
-    id: 2,
-    title: "Python Hello World",
-    language: "Python",
-    code: 'print("Hello, World!")',
-  },
-  {
-    id: 3,
-    title: "HTML Button",
-    language: "HTML",
-    code: '<button class="btn btn-primary">Click Me</button>',
-  },
-];
+import { useSelector } from "react-redux";
 
 const SnippetsList = () => {
+  const {loggedInUser} = useSelector((store)=>store.auth);
   return (
     <>
       <Navbar />
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">Saved Code Snippets</h1>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {snippets.map((snippet) => (
-            <SnippetCard key={snippet.id} snippet={snippet} />
+          {loggedInUser?.snippets.map((snippet) => (
+            <SnippetCard key={snippet?._id} snippet={snippet} />
           ))}
         </div>
       </div>
